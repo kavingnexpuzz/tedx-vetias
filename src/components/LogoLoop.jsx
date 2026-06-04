b93657cd60e647af391c0a68a5fa196351aecc91
Kavin.G <gkavin890@gmail.com>
2026-06-04 15:44:04 +0530

    vercel change

--- src/components/LogoLoop.jsx
+++ src/components/LogoLoop.jsx
@@ -273,7 +273,7 @@ export const LogoLoop = memo(
       (item, key) => {
         if (renderItem) {
           return (
-            <li className="logoloop__item" key={key} role="listitem">
+            <li className="logoloop__item" key={key} >
               {renderItem(item, key)}
             </li>
           );
@@ -317,7 +317,7 @@ export const LogoLoop = memo(
           content
         );
         return (
-          <li className="logoloop__item" key={key} role="listitem">
+          <li className="logoloop__item" key={key}>
             {itemContent}
           </li>
         );
@@ -331,7 +331,6 @@ export const LogoLoop = memo(
           <ul
             className="logoloop__list"
             key={`copy-${copyIndex}`}
-            role="list"
             aria-hidden={copyIndex > 0}
             ref={copyIndex === 0 ? seqRef : undefined}
           >
